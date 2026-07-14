cd23036623dd6c17621650e05ec75d69d2661ce7
Manuele023 <manueletrajano2011@gmail.com>
2026-07-14 19:07:59 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,28 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
 import { 
-    getFirestore 
+getFirestore 
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-import {
-    getAuth
+import { 
+getAuth 
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyD7-5DJY3dGOiq71vgcpWJQgbWM9HOA2Ek",
+
   authDomain: "lavagem-cartel.firebaseapp.com",
+
   projectId: "lavagem-cartel",
+
   storageBucket: "lavagem-cartel.appspot.com",
+
   messagingSenderId: "35926730423",
+
   appId: "1:35926730423:web:2b7e72ee07fb76cd358f32"
+
 };
 
 
@@ -23,5 +30,6 @@ const app = initializeApp(firebaseConfig);
 
 
 export const db = getFirestore(app);
+
 
 export const auth = getAuth(app);
